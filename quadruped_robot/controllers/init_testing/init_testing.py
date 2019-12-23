919ed7aee0b22d22dbd0bf4aa99cad62c622a3b1
lihaoxiang1989 <hxl.colin@gmail.com>
2019-12-23 15:41:28 +0800

    step on without controllers

--- quadruped_robot/controllers/init_testing/init_testing.py
+++ quadruped_robot/controllers/init_testing/init_testing.py
@@ -1,8 +1,27 @@
 """init_testing controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import math
+import numpy as np
+
+def setPositions(motors, theta, posSensors):
+  delta = 0.018
+  for i in range(len(motors)):
+    motors[i].setPosition(theta[i])
+
+  while robot.step(timestep) != -1:
+    checked = True
+    for i in range(len(motors)):
+      effective = posSensors[i].getValue()
+      target = theta[i]
+      
+      if math.fabs(effective - target) > delta:
+        print('effective: ', effective)
+        print('target', target)
+        checked = False
+    if checked:
+      break
+    
 
 # create the Robot instance.
 robot = Robot()
@@ -10,23 +29,60 @@ robot = Robot()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
+# init 12 DOF motors and position sensors
+motorNames = [
+  'hip0x', 'hip0z', 'knee0',
+  'hip1x', 'hip1z', 'knee1',
+  'hip2x', 'hip2z', 'knee2',
+  'hip3x', 'hip3z', 'knee3'
+]
+motors = [
+  robot.getMotor(motorNames[i]) for i in range(len(motorNames))
+]
+posSensors = [
+  robot.getPositionSensor(motorNames[i] + '_pos') for i in range(len(motorNames))
+]
+for posSensor in posSensors:
+  posSensor.enable(timestep)
+
+# init positions
+thetaInit = np.array([
+  0, -45/180 * math.pi, 90/180 * math.pi,
+  0, -45/180 * math.pi, 90/180 * math.pi,
+  0, 45/180 * math.pi, -90/180 * math.pi,
+  0, 45/180 * math.pi, -90/180 * math.pi
+])
+setPositions(motors, thetaInit, posSensors)
+
+isLeftStepUp = True
+leftStepUp = [
+  0, -45/180 * math.pi, 90/180 * math.pi,
+  0, -60/180 * math.pi, 120/180 * math.pi,
+  0, 60/180 * math.pi, -120/180 * math.pi,
+  0, 45/180 * math.pi, -90/180 * math.pi
+]
+rightStepUp = [
+  0, -60/180 * math.pi, 120/180 * math.pi,
+  0, -45/180 * math.pi, 90/180 * math.pi,
+  0, 45/180 * math.pi, -90/180 * math.pi,
+  0, 60/180 * math.pi, -120/180 * math.pi
+]
+
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+  if isLeftStepUp == True:
+    setPositions(motors, leftStepUp, posSensors)
+  else:
+    setPositions(motors, rightStepUp, posSensors)
+  isLeftStepUp = not isLeftStepUp
+  
 
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+  pass
 
 # Enter here exit cleanup code.
+
+
+
+
