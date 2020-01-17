45e8da9b3c4b5695c32c4c5070aea27599444701
lihaoxiang1989 <hxl.colin@gmail.com>
2020-01-17 10:49:12 +0800

    calculate the joint angles

---
+++ mathematics/inverse_kinematic/trajectory.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+
+targetPoints = pd.read_csv('./results/targetPoints').to_numpy()
+link1 = 11
+link2 = 9
+
+initAng = []
+
+theta = []
+
+# calculate the position of knee joint and R of end effect
+for tx, ty in targetPoints:
+    d = np.sqrt(tx ** 2 + ty ** 2)
+    v = (link1 ** 2 + d ** 2 - link2 ** 2) / (2 * link1 * d)
+
+    if v > 1:
+        print('theta error')
+        hTheta1 = 0
+    else:
+        hTheta1 = np.arccos((link1 ** 2 + d ** 2 - link2 ** 2) / (2 * link1 * d))
+
+    theta3 = np.arccos((link2 ** 2 + d ** 2 - link1 ** 2) / (2 * link2 * d))
+    hTheta2 = np.arctan(tx / ty)
+    # preset theta0
+    theta0 = np.arctan(0.5)
+    theta.append([theta0, hTheta1 + hTheta2, hTheta1 + theta3])
+
+theta = np.array(theta)
+theta1 = theta[..., 1]
+
+output = pd.DataFrame({
+    'theta0': theta[..., 0],
+    'theta1': theta[..., 1],
+    'theta2': theta[..., 2]
+})
+output.to_csv('./results/thetaList', index=False)
