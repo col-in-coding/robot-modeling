da635d9c6a4c790dcccc029987c0710d59599d4f
lihaoxiang1989 <hxl.colin@gmail.com>
2020-02-06 13:03:57 +0800

    write into c++ lib

--- hardware_test/singleLeg/Robot3R.cpp
+++ hardware_test/singleLeg/Robot3R.cpp
@@ -4,54 +4,65 @@
  * Source Code
  *******************************************************************/
 
-#include "Robot3R.h"
 #include "Arduino.h"
+#include "Robot3R.h"
 #include <Servo.h>
 
-Robot3R::Robot3R() {
-  for (int i = 0; i < dof; i++) {
-    hip0.attach(11);
-    hip1.attach(10);
-    knee.attach(9);
-  }
+// servos
+Servo hip0_servo;
+Servo hip1_servo;
+Servo knee_servo;
+
+Robot3R::Robot3R(int hip0_p, int hip1_p, int knee_p) {
+  hip0_pin = hip0_p;
+  hip1_pin = hip1_p;
+  knee_pin = knee_p;
 }
 
-Robot3R::~Robot3R() {
-}
-
-void Robot3R::servo_set_us(int servo_id, int us) {
-  switch (servo_id) {
-   case 0:
-    hip0.writeMicroseconds(us);
-    break;
-   case 1:
-    hip1.writeMicroseconds(us);
-    break;
-   case 2:
-    knee.writeMicroseconds(us);
-    break;
-  }
+void Robot3R::start() {
+  Serial.println("start robot in 3 second:");
+  hip0_servo.attach(hip0_pin);
+  hip1_servo.attach(hip1_pin);
+  knee_servo.attach(knee_pin);
   delay(1000);
+  servo_set_angs(current_theta);
+  delay(2000);
 }
 
-void Robot3R::set_joint_angles(int theta [3]) {
-  for (int i = 0; i < dof; i++) {
-    int servo_ang = theta[i] + init_servo_angles[i];
-    int us = servo_us_min + servo_ang * us_per_deg;
-    Serial.println(us);
-    servo_set_us(i, us);
+/**
+ * Convert Angle Radius to PWM us
+ */
+int Robot3R::convert_rad_to_pulse_us(double rad) {
+  int us = SERVO_US_MIN + (SERVO_US_MAX - SERVO_US_MIN) * rad / 3.14;
+  return us;
+}
+
+/**
+ * Convert Angle Degree to PWM us
+ */
+int Robot3R::convert_ang_to_pulse_us(int ang) {
+  int us = SERVO_US_MIN + static_cast<double>(SERVO_US_MAX - SERVO_US_MIN) * ang / 180;
+  return us;
+}
+
+void Robot3R::servo_set_angs(int angs[]) {
+  int ang0 = INIT_ANG[0] + angs[0];
+  int ang1 = INIT_ANG[1] + angs[1];
+  int ang2 = INIT_ANG[2] - angs[2];
+
+  hip0_servo.writeMicroseconds(convert_ang_to_pulse_us(ang0));
+  hip1_servo.writeMicroseconds(convert_ang_to_pulse_us(ang1));
+  knee_servo.writeMicroseconds(convert_ang_to_pulse_us(ang2));
+  delayMicroseconds(1000);
+}
+
+void Robot3R::run_period() {
+  for (int i = 0; i < 100; ++i){
+    for (int j = 0; j < 3; ++j) {
+      current_theta[j] = task[i][j];
+    }
+    
+    servo_set_angs(current_theta);
+    delay(50);
   }
-}
-
-void Robot3R::set_joint_angles_sequence() {
-
-}
-
-void Robot3R::run() {
-  
-}
-
-void Robot3R::start(){
-  int setup_ang [3] {0, 0, 0};
-  set_joint_angles(setup_ang);
 }
--- hardware_test/singleLeg/Robot3R.h
+++ hardware_test/singleLeg/Robot3R.h
@@ -3,50 +3,132 @@
  * 3R Robot
  * Header file
  *******************************************************************/
+#ifndef ROBOT_3R_H
+#define ROBOT_3R_H
 
-#include <Servo.h>
-#include <Arduino.h>
-
-// servos
-Servo hip0;
-Servo hip1;
-Servo knee;
-
-const int servo_us_min {500};
-const int servo_us_max {2500};
-// hip joint 0, hip joint 1, knee joint
-const int servo_pins [3] {11, 10, 9};
-const float us_per_deg {11.1111111};
+const int SERVO_US_MIN {500};
+const int SERVO_US_MAX {2500};
+// set init angles as the base angles
+const int INIT_ANG[3] {45, 90, 180};
 
 class Robot3R
 {
 private:
-  const int dof {3};
-  // servos configs: servo angles to align the coordinate
-  int init_servo_angles [3] {45, 90, 45};
-  bool motor_rot_reverse [3] {1, 0, 0};
+  int hip0_pin {};
+  int hip1_pin {};
+  int knee_pin {};
+  int current_theta [3] {0, 0, 0};
+  int task[100][3]
+  {
+    {0,16,36},
+    {0,17,36},
+    {0,17,36},
+    {0,18,36},
+    {0,19,36},
+    {0,19,35},
+    {0,20,35},
+    {0,20,35},
+    {0,21,34},
+    {0,22,34},
+    {0,22,33},
+    {0,22,32},
+    {0,23,31},
+    {0,23,31},
+    {0,23,30},
+    {0,24,29},
+    {0,24,27},
+    {0,24,26},
+    {0,24,25},
+    {0,24,23},
+    {0,24,21},
+    {0,24,19},
+    {0,23,17},
+    {0,23,14},
+    {0,22,9},
+    {0,22,9},
+    {0,27,22},
+    {0,30,30},
+    {0,33,36},
+    {0,35,41},
+    {0,37,46},
+    {0,38,50},
+    {0,39,54},
+    {0,40,57},
+    {0,41,60},
+    {0,42,63},
+    {0,42,66},
+    {0,43,68},
+    {0,43,70},
+    {0,43,73},
+    {0,43,74},
+    {0,43,76},
+    {0,42,78},
+    {0,42,79},
+    {0,41,80},
+    {0,41,81},
+    {0,40,82},
+    {0,39,82},
+    {0,38,83},
+    {0,37,83},
+    {0,35,83},
+    {0,34,83},
+    {0,33,82},
+    {0,31,82},
+    {0,30,81},
+    {0,29,80},
+    {0,27,79},
+    {0,25,78},
+    {0,24,76},
+    {0,22,74},
+    {0,21,73},
+    {0,19,70},
+    {0,17,68},
+    {0,15,66},
+    {0,14,63},
+    {0,12,60},
+    {0,10,57},
+    {0,8,54},
+    {0,6,50},
+    {0,4,46},
+    {0,1,41},
+    {0,0,36},
+    {0,-3,30},
+    {0,-7,22},
+    {0,-13,9},
+    {0,-13,9},
+    {0,-10,14},
+    {0,-8,17},
+    {0,-6,19},
+    {0,-5,21},
+    {0,-3,23},
+    {0,-2,25},
+    {0,0,26},
+    {0,0,27},
+    {0,1,29},
+    {0,3,30},
+    {0,4,31},
+    {0,5,31},
+    {0,6,32},
+    {0,7,33},
+    {0,8,34},
+    {0,9,34},
+    {0,10,35},
+    {0,11,35},
+    {0,12,35},
+    {0,13,36},
+    {0,14,36},
+    {0,14,36},
+    {0,15,36},
+    {0,16,36},
+  };
 
 
 public:
-  Robot3R(); // constructor
-  ~Robot3R(); // distructor
-
-  void Robot3R::servo_set_us(int servo_id, int us);
-
-  /**
-   * assign a single line task
-   */
-  void set_joint_angles(int angles [3]);
-
-  /**
-   * assign a new task sequence
-   */
-  void set_joint_angles_sequence();
-
-  /**
-   * actuation
-   */
-  void run();
-
+  Robot3R(int hip0_pin, int hip1_pin, int knee_pin);
   void start();
+  int convert_rad_to_pulse_us(double rad);
+  int Robot3R::convert_ang_to_pulse_us(int ang);
+  void servo_set_angs(int angs[]);
+  void run_period();
 };
+#endif
