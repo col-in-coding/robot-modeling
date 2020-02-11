8931122f8dab8d031e2aa44d33ecdb488065a131
lihaoxiang1989 <hxl.colin@gmail.com>
2020-02-11 18:03:50 +0800

    Two Joint Leg Forward Kinematic Formulas

---
+++ mathematics/inverse_kinematic/FK_2joints.py
@@ -0,0 +1,21 @@
+###
+# Two joints Leg Forward kinematic
+#
+# Inputs:
+#   l1: link1 length
+#   l2: link2 length
+#   (a, b): end effect coordinate
+#
+# Output:
+#   T: Homogeneous Transformation Matrix of End Effect
+#
+# Formulas:
+#   a = c01 * x12 + s01 * y12 + x01
+#   b = -s01 * x12 + c01 * y12 + y01
+#   l1 ** 2 = x01 ** 2 + y01 ** 2
+#   l2 ** 2 = x12 ** 2 + y12 ** 2
+#   theta01 = arctan(y01 / x01)
+#   theta12 = arctan(y12 / x12)
+#   theta02 = theta01 + theta12
+###
+
