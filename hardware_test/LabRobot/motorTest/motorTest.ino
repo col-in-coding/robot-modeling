dd8279a93f50c8a9a5975a01171b0b5ebcb2628f
lihaoxiang1989 <hxl.colin@gmail.com>
2020-03-05 10:29:53 +0800

    Lab robot single servo motor test

--- hardware_test/LabRobot/motorTest/motorTest.ino
+++ hardware_test/LabRobot/motorTest/motorTest.ino
@@ -1,9 +1,34 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+// our servo # counter
+uint8_t servonum = 5;
+Servo s;
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Lab robot servo test!");
+  s.attach(servonum, 500, 2500);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void writeServo(uint8_t angle) {
+  double pulse;
+  pulse = (0.5 + angle/90.0) * 1000;
+  Serial.print("pulse:");
+  Serial.println(pulse);
+  s.writeMicroseconds(pulse);
+}
 
+String inString = "";
+void loop() {
+  if (Serial.available() > 0) {
+    if (Serial.peek() != '\n') {
+      inString += (char)Serial.read();
+    } else {
+      Serial.read();
+      Serial.print("Instruction received: ");
+      Serial.println(inString);
+      writeServo(inString.toInt());
+      inString = "";
+    }
+  }
 }
