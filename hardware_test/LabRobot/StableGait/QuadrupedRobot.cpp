634723b9fa3c3a58d23c8fc0fc6f465783623f9e
lihaoxiang1989 <hxl.colin@gmail.com>
2020-03-31 19:46:54 +0800

    Revert "Lab Robot Non-periodic Gaits, Forward Kinematics test"
    This reverts commit 5908ca432bab9b6615ab6d9253ceb3ffb7d9fa07.

--- hardware_test/LabRobot/StableGait/QuadrupedRobot.cpp
+++ hardware_test/LabRobot/StableGait/QuadrupedRobot.cpp
@@ -1,6 +1,5 @@
 
 #include <Arduino.h>
-#include <math.h>
 #include "QuadrupedRobot.h"
 
 /**
@@ -118,16 +117,17 @@ void QuadrupedRobot::servo_move(float *angs_ptr)
     {
       timenow = millis();
       joint_angs_now[i] += joint_degs_speed[i] * (timenow - servo_time[i]);
-      servo_time[i] = timenow; // update servo time
     }
-    servo_write_angs(joint_angs_now, false);
+    servo_write_angs(joint_angs_now);
     timenow = millis();
   }
   // last run to assure that all servo has been desired position
   for (size_t i = 0; i < DOF; i++)
   {
-    servo_write_angs(angs_ptr, false);
+    servo_write_angs(angs_ptr);
   }
+  
+  
 }
 
 /**
@@ -135,19 +135,13 @@ void QuadrupedRobot::servo_move(float *angs_ptr)
  * Current Angles is in Callibrated Coordinate,
  * which should be transfered to real angle degrees before using.
  */
-void QuadrupedRobot::servo_write_angs(float *angs_ptr, bool is_in_degrees)
+void QuadrupedRobot::servo_write_angs(float *angs_ptr)
 {
   float real_ang{};
   float pulsewidth{};
-  float ang_in_degree{};
   for (size_t i = 0; i < DOF; i++)
   {
-    if (is_in_degrees){
-      ang_in_degree = angs_ptr[i];
-    } else {
-      ang_in_degree = angs_ptr[i] * 180 / M_PI;
-    }
-    real_ang = init_servo_deg[i] + servo_dir[i] * ang_in_degree;
+    real_ang = init_servo_deg[i] + servo_dir[i] * angs_ptr[i];
     pulsewidth = 500 + real_ang / 90.0 * 1000;
     if (!servos_ptr[i]->attached())
     {
@@ -155,120 +149,8 @@ void QuadrupedRobot::servo_write_angs(float *angs_ptr, bool is_in_degrees)
     }
     servos_ptr[i]->writeMicroseconds(pulsewidth);
     // update current angles buffer
-    if (is_in_degrees) {
-      joint_angs_pre[i] = angs_ptr[i] * M_PI / 180;
-    } else {
-      joint_angs_pre[i] = angs_ptr[i];
-    }
+    joint_angs_pre[i] = angs_ptr[i];
   }
-}
-
-/**
- * Moving body by moving the feet in opposite direction
- * Body Frame xyz
- */
-void QuadrupedRobot::body_xyz(float x, float y, float z)
-{
-  for (size_t i = 0; i < 4; i++)
-  {
-    foot_pos[3 * i] = -x;
-    foot_pos[3 * i + 1] = -y;
-    foot_pos[3 * i + 2] = -z;
-  }
-  inverse_kinematics();
-  servo_move(joint_angs_new);
-}
-
-/**
- * Calculate the joint angles by foot positions
- */
-void QuadrupedRobot::inverse_kinematics()
-{
-  // FL 0, 1, 2
-  joint_angs_new[0] = gamma_left(foot_pos[1], foot_pos[2]); // HipX
-  vlegs_len[0] = vleg_left(foot_pos[0], foot_pos[1], joint_angs_new[0]);
-  joint_angs_new[2] = beta_front(vlegs_len[0]);
-  joint_angs_new[1] = alfa_front(foot_pos[0], joint_angs_new[2], vlegs_len[0]);
-  // FR 3, 4, 5
-  joint_angs_new[3] = gamma_right(foot_pos[4], foot_pos[5]);
-  vlegs_len[3] = vleg_right(foot_pos[3], foot_pos[4], joint_angs_new[3]);
-  joint_angs_new[5] = beta_front(vlegs_len[3]);
-  joint_angs_new[4] = alfa_front(foot_pos[3], joint_angs_new[5], vlegs_len[3]);
-  // RR 6, 7, 8
-  joint_angs_new[6] = gamma_right(foot_pos[7], foot_pos[8]);
-  vlegs_len[6] = vleg_right(foot_pos[6], foot_pos[7], joint_angs_new[6]);
-  joint_angs_new[8] = beta_rear(vlegs_len[6]);
-  joint_angs_new[7] = alfa_rear(foot_pos[6], joint_angs_new[8], vlegs_len[6]);
-  // RL 9, 10, 11
-  joint_angs_new[9] = gamma_left(foot_pos[10], foot_pos[11]);
-  vlegs_len[9] = vleg_left(foot_pos[9], foot_pos[10], joint_angs_new[9]);
-  joint_angs_new[11] = beta_rear(vlegs_len[9]);
-  joint_angs_new[10] = alfa_rear(foot_pos[9], joint_angs_new[11], vlegs_len[9]);
-}
-
-// Gamma: Hip Z angle
-float QuadrupedRobot::gamma_left(float dy, float dz)
-{
-  float res = atan((toe_out0 - dz) / (height0 - dy)) - gamma0;
-  return res;
-}
-
-float QuadrupedRobot::gamma_right(float dy, float dz)
-{
-  float res = gamma0 - atan((toe_out0 + dz) / (height0 - dy));
-  return res;
-}
-// virtual leg length
-float QuadrupedRobot::vleg_left(float dx, float dy, float gamma)
-{
-  float res = sqrt(pow(vleg_len0 - (dy / cos(gamma0 + gamma)), 2) + pow(dx, 2));
-  if (res > thigh + calf)
-    res = thigh + calf;
-  return res;
-}
-
-float QuadrupedRobot::vleg_right(float dx, float dy, float gamma)
-{
-  float res = sqrt(pow(vleg_len0 - (dy / cos(gamma0 - gamma)), 2) + pow(dx, 2));
-  if (res > thigh + calf)
-    res = thigh + calf;
-  return res;
-}
-
-float QuadrupedRobot::beta_front(float vleg_len)
-{
-  float res = M_PI - acos(
-      (thigh * thigh + calf * calf - vleg_len * vleg_len) /
-      (2 * thigh * calf));
-  return res;
-}
-
-float QuadrupedRobot::beta_rear(float vleg_len)
-{
-  float res = acos(
-      (thigh * thigh + calf * calf - vleg_len * vleg_len) /
-      (2 * thigh * calf)) - M_PI;
-  return res;
-}
-
-float QuadrupedRobot::alfa_front(float dx, float beta, float vleg_len)
-{
-  float res = asin(dx / vleg_len);
-  res -= acos(
-    (thigh * thigh + vleg_len * vleg_len - calf * calf) /
-    (2 * thigh * vleg_len)
-  );
-  return res;
-}
-
-float QuadrupedRobot::alfa_rear(float dx, float beta, float vleg_len)
-{
-  float res = asin(dx / vleg_len);
-  res += acos(
-    (thigh * thigh + vleg_len * vleg_len - calf * calf) /
-    (2 * thigh * vleg_len)
-  );
-  return res;
 }
 
 /**
@@ -276,7 +158,7 @@ float QuadrupedRobot::alfa_rear(float dx, float beta, float vleg_len)
  */
 void QuadrupedRobot::adjust()
 {
-  servo_write_angs(adjust_angs, true);
+  servo_write_angs(adjust_angs);
   clear_cmd();
 }
 
@@ -286,7 +168,7 @@ void QuadrupedRobot::adjust()
  */
 void QuadrupedRobot::bot_stand()
 {
-  servo_write_angs(stand_angs, true);
+  servo_write_angs(stand_angs);
   clear_cmd();
 }
 
@@ -296,7 +178,7 @@ void QuadrupedRobot::bot_stand()
  */
 void QuadrupedRobot::bot_rest()
 {
-  servo_write_angs(rest_angs, true);
+  servo_write_angs(rest_angs);
   clear_cmd();
 }
 
@@ -306,9 +188,7 @@ void QuadrupedRobot::bot_rest()
  */
 void QuadrupedRobot::bot_walk()
 {
-  Serial.println("walk");
-  body_xyz(0, 0, 0);
-  clear_cmd();
+  Serial.print("walk");
 }
 
 QuadrupedRobot::~QuadrupedRobot()
--- hardware_test/LabRobot/StableGait/QuadrupedRobot.h
+++ hardware_test/LabRobot/StableGait/QuadrupedRobot.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 #include <Servo.h>
-#include <math.h>
 
 #define PT(s) Serial.print(s)
 #define PTL(s) Serial.println(s)
@@ -32,67 +31,26 @@ private:
       6, 7, 8,
       9, 10, 11,
       12, 13, 14};
-  // Time taken by each sequence, used in servo_move()
+  // Time taken by each sequence, used in servo_write_angs()
   unsigned long timestep = 500;
-  int steplen = 40;
 
-  /**
-   * Servo rotation configs
-   * Real servo coord to calibrated coord: X-roll, Y-yaw, Z-pitch
-   */
-  const float init_servo_deg[12]{35, 90, 180, 125, 83, 0, 40, 85, 180, 127, 83, 0};
-  const int8_t servo_dir[12]{-1, -1, -1, -1, 1, 1, -1, 1, 1, -1, -1, -1};
-  const float toe_out0{30}; // outward distance of toe during stand, in mm
-  const float dist_ag{30};  // distance between alfa and gamma axis, in mm
-  const float thigh{107};
-  const float calf{90};
-  /**
-   * vleg_len: virtual leg length (composite with 2 limbs)
-   * alfa, gamma, beta is conresponding to angles on HipZ, Knee, HipX axises
-   */
-  const float vleg_len0{160};
-  const float height0{sqrt(pow(vleg_len0 + dist_ag, 2) - pow(toe_out0, 2))};
-  const float alfa0{acos((thigh * thigh + vleg_len0 * vleg_len0 - calf * calf) / (2 * thigh * vleg_len0))};
-  const float beta0{M_PI - acos((thigh * thigh + calf * calf - vleg_len0 * vleg_len0) / (2 * thigh * calf))};
-  const float gamma0{asin(toe_out0 / (vleg_len0 + dist_ag))};
-  /**
-   * State Variables
-   * angles in radiums
-   * Foot position, order: FLxyz, FRxyz, RLxyz, RRxyz
-   */
+  // Servo rotation configs
+  const float init_servo_deg[12]{35, 90, 180, 130, 83, 0, 35, 85, 180, 130, 83, 0};
+  const int8_t servo_dir[12]{1, -1, -1, -1, 1, 1, -1, 1, 1, 1, -1, -1};
+  
+  // float joint_angs_now[12]{}; //Current joint angles, in degrees
   float joint_angs_pre[12]{}; //Previous joint angles
-  float joint_angs_new[12]{};
-  float vlegs_len[4]{};
-  float foot_pos[12] {};
-  /**
-   * Configed States
-   * Angles in callibrated coordinate
-   */
+
+  // Angles in callibrated coordinate
   const float stand_angs[12]{0, -30, 60, 0, -30, 60, 0, 30, -60, 0, 30, -60};
   const float rest_angs[12]{0, -55, 130, 0, -55, 130, 0, 55, -130, 0, 55, -130};
   const float adjust_angs[12] {};
 
-  // 
-  // X points to forward, Z points to upward
-  
-  
   void setup_servos();
   void shut_servos();
   void servo_move(float *angs_ptr);
-  void servo_write_angs(float *angs_ptr, bool is_in_degrees);
+  void servo_write_angs(float *angs_ptr);
   void clear_cmd();
-
-  void body_xyz(float x, float y, float z);
-
-  void inverse_kinematics();
-  float gamma_left(float dy, float dz);
-  float gamma_right(float dy, float dz);
-  float vleg_left(float dx, float dy, float gamma);
-  float vleg_right(float dx, float dy, float gamma);
-  float beta_front(float vleg_len);
-  float beta_rear(float vleg_len);
-  float alfa_front(float dx, float beta, float vleg_len);
-  float alfa_rear(float dx, float beta, float vleg_len);
 
 public:
   ~QuadrupedRobot();
